ed377a81f937770d7c1a8543d5fdd9cc5bbba1ed
Nicholas Renninger <nire1188@colorado.edu>
2019-05-08 04:21:09 -0600

    stubbing out the classes for the automata and occupancy set

---
+++ DFA.py
@@ -0,0 +1,38 @@
+class DFA:
+    # @brief      A class representing a Deterministic Finite Automata
+
+    # @brief      Constructs the DFA object.
+    #
+    # @param      self       The object instance reference
+    # @param      nodes      A list of Node objects making up the DFA
+    # @param      startNode  The initializing Node object
+    # @param      transFcn   The transaction function for the DFA
+    # @param      accepts    A list of indices into nodes for Nodes that accept
+    #
+    def __init__(self, nodes, startNode, transFcn, accepts):
+
+        self.nodes = nodes
+        self.startNode = startNode
+        self.transFcn = transFcn
+        self.accepts = accepts
+
+
+#
+# @brief      This functions forms the product automata between a deterministic
+#     Buchi automata and a deterministic finite transition system.transition
+#
+# As forming the product involves using a graph search (here the graph search
+# is based on BFS), we simply instrument this BFS search to build up a path
+# through the product, and return once it finds the first accepting state. As
+# this graph is a DAG, BFS produces the shortest path through the graph and
+# thus this trace in the product is actually the optimal controller.
+#
+# @param      TS    A TransistionSystem to product with the LBDA, containing
+#     the physical modeling transitions
+# @param      LDBA  The LDBA (LTL Deterministic Buchi Automata) encoding the
+#     LTL specification on TS
+#
+# @return     the first Node object in the product to accept
+#
+def formAndSolveProduct(TS, LDBA):
+    return 1
---
+++ TransistionSystem.py
@@ -0,0 +1,135 @@
+class TransitionSystem:
+    # @brief    A class representing a transition system DFA abstraction
+    #
+    # For our purposes, this class represents the car-road transition system,
+    # with all possibile lane changes and choices of velocity allowed
+
+    #
+    # @brief      Constructs the TransitionSystem object.
+    #
+    # @param      self           The TransitionSystem object instance
+    # @param      initCarX       The initial carX state. CarX ~ lane on highway
+    # @param      initCarY       The initial carY state. CarY ~ distance down
+    #                            highway
+    # @param      initCarT       The initial carT state. CarT ~ time step
+    # @param      maxTime        The maximum time step allowed
+    # @param      allLanes       A list of all possible lane numbers on the
+    #                            highway.
+    # @param      allVelocities  A list of all possible car velocities for ALL
+    #                            lanes
+    #
+    def __init__(self, initCarX, initCarY, initCarT, maxTime,
+                 allLanes, allVelocities):
+
+        return 1
+
+    #
+    # @brief      Calculates a boolean for whether the car is speeding
+    #
+    #             This boolean flag is used as one of the observations of the
+    #             TransitionSystem itself.
+    #
+    # @param      self                 The TransitionSystem object instance
+    # @param      prevLane             The lane the car was in during the
+    #                                  previous time step
+    # @param      prevVel              The previous velocity the car was
+    #                                  traveling at during the previous time
+    #                                  step
+    # @param      carX                 The current carX value ~ lane on highway
+    # @param      allowedVelsPrevLane  The set of legal velocities allowed in
+    #                                  the prevLane lane
+    # @param      allowedVelsCarXLane  The set of legal velocities allowed in
+    #                                  the carX lane
+    #
+    # @return     @bool indicating whether or not the car was / is speeding in
+    #             the prior or current time step
+    #
+    def speeding(self, prevLane, prevVel, carX, allowedVelsPrevLane,
+                 allowedVelsCarXLane):
+
+        if (prevVel in allowedVelsPrevLane) and \
+           (prevVel in allowedVelsCarXLane):
+            return False
+        else:
+            return True
+
+    #
+    # @brief      Calculates a boolean for whether the car is in one of the
+    #             goal states along the road.
+    #
+    #             A goal state is a lane (x) and horizontal distance down the
+    #             highway from the starting location (Y). These correspond to
+    #             an exit on the highway the driver would like to use. This
+    #             boolean flag is used as one of the observations of the
+    #             TransitionSystem itself.
+    #
+    # @param      self        The TransitionSystem object instance
+    # @param      carX        The current carX value ~ lane on highway
+    # @param      carY        The current carY value ~ distance down highway
+    # @param      goalStates  The set of goal states in x and y
+    #
+    # @return     @bool indicating whether or not the car is in one of the set
+    #             of goalStates during current time step
+    #
+    def inGoalStates(self, carX, carY, goalStates):
+
+        if (carX in goalStates) and (carY in goalStates):
+            return True
+        else:
+            return False
+
+    #
+    # @brief      Calculates a boolean for whether the car has crashed into
+    #             another one of the obstacle cars along the highway
+    #
+    #             This boolean flag is used as one of the observations of the
+    #             TransitionSystem itself.
+    #
+    # @param      self                The TransitionSystem object instance
+    # @param      prevLane            The previous lane for the car during the
+    #                                 last time step
+    # @param      prevVel             The previous velocity for the car during
+    #                                 the last time step
+    # @param      carX                The current carX value ~ lane on highway
+    # @param      carY                The current carY value ~ distance down
+    #                                 highway
+    # @param      carT                The current carT value ~ current time
+    #                                 step
+    # @param      minSpeedInPrevLane  The minimum legal speed in the previous
+    #                                 lane
+    # @param      minSpeedInCarXLane  The minimum legal speed in the carX lane
+    # @param      POS                 The POS (physical occupancy set) object
+    #                                 which contains the 3D projection of a
+    #                                 Node state onto the x, y, and time grid
+    #                                 for the empty road.
+    #
+    # @return     @bool indicating whether or not the car will crash into
+    #             another driver during the previous -> current time step if
+    #             they take a certain control action
+    #
+    def crashed(self, prevLane, prevVel, carX, carY, carT,
+                minSpeedInPrevLane, minSpeedInCarXLane, POS):
+
+        # time steps are unit length for simplification
+        timeStepLength = 1
+        prevY = carY - prevVel * (timeStepLength)
+        prevT = carT - timeStepLength
+
+        # POS[x, y, t] = True (1) if another car is at the specific (x,y)
+        # location of the road at time t
+        distDownTheHwy = prevVel - minSpeedInPrevLane
+        sumPrevLane = 0
+        for ii in range(0, distDownTheHwy):
+            sumPrevLane += POS[prevLane, prevY + ii, prevT]
+
+        distDownTheHwy = prevVel - minSpeedInCarXLane
+        sumCarXLane = 0
+        for ii in range(0, distDownTheHwy):
+            sumCarXLane += POS[carX, prevY + ii, prevT]
+
+        numCollisions = sumPrevLane + sumCarXLane
+
+        if numCollisions > 0:
+            return True
+        else:
+            return False
