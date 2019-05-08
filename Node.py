927a892ebdd63c359f815fedf40d5c0b7b979249
Nicholas Renninger <nire1188@colorado.edu>
2019-05-08 07:10:17 -0600

    adding Node observation class

--- Node.py
+++ Node.py
@@ -15,7 +15,7 @@ class NodeState:
     # @param      prevVel   The previous velocity for the car during the last
     #                       time step
     #
-    def __init__(self, carX, carY, carT, q, prevLane, prevVel):
+    def __init__(self, carX, carY, carT, q=None, prevLane=None, prevVel=None):
 
         self.carX = carX
         self.carY = carY
@@ -23,6 +23,28 @@ class NodeState:
         self.q = q
         self.prevLane = prevLane
         self.prevVel = prevVel
+
+
+class Observation:
+    # @brief this is just an enum / struct for the three different observations
+
+    #
+    # @brief      Constructs the Observation object.
+    #
+    # @param      self      The Observation object instance
+    # @param      atGoal    @bool for inidicating the NodeState is in the goal
+    #                       state
+    # @param      crashed   @bool for inidicating the NodeState is in a state
+    #                       that has collided with an obstacle in the POS
+    # @param      speeding  @bool for inidicating the NodeState is such that
+    #                       the current velocity exceeds the defined maximum
+    #                       allowable velocities
+    #
+    def __init__(self, atGoal, crashed, speeding):
+
+        self.atGoal = atGoal
+        self.crashed = crashed
+        self.speeding = speeding
 
 
 class Node:
@@ -37,8 +59,8 @@ class Node:
     # @param      state        A reference to the NodeState object, containing
     #                          the state info for the node
     # @param      index        The node index (numerical, unique id)
-    # @param      observation  A tuple of booleans for the three observations a
-    #                          Node in this project can have:
+    # @param      obs          An Observation object holding the three
+    #                          observations a Node in this project can have:
     #                          1) 'atGoal': @bool
     #                          2) 'crashed': @bool
     #                          3) 'speeding': @bool
@@ -48,12 +70,12 @@ class Node:
     # @param      isVisited    Indicates if this Node has been visited during a
     #                          graph search
     #
-    def __init__(self, state, index, observation={}, adjList=[],
+    def __init__(self, state, index, obs=None, adjList=[],
                  isAccepting=False, isVisited=False):
 
         self.state = state
         self.index = index
-        self.observation = observation
+        self.obs = obs
         self.adjList = adjList
         self.isAccepting = isAccepting
         self.isVisited = isVisited
