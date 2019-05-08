3879324eedaf4ea09a3dac5be979438ef0b42893
Nicholas Renninger <nire1188@colorado.edu>
2019-05-08 06:05:40 -0600

    adding occupancy set definition

---
+++ POS.py
@@ -0,0 +1,33 @@
+import numpy
+import random
+
+
+def makePOS(allLanes, allowedLaneVelocites, maxDist, maxTime,
+            initCarX, initCarY):
+
+    numLanes = max(allLanes)
+    POS = numpy.zeros(numLanes, maxDist, maxTime)
+
+    for ii in range(0, numLanes):
+
+        # generating the spacing for the random other cars
+        currLaneVelocityRange = allowedLaneVelocites[ii]
+        minAllowedVelInLane = min(currLaneVelocityRange)
+        dist = random.randint(0, minAllowedVelInLane * 2)
+
+        # generating the obstacles for one time slice
+        while dist < maxDist:
+            POS[ii, dist, 0] = 1
+            dist = dist + random.randint(0, minAllowedVelInLane * 2)
+
+        # make sure to delete any other car that happens to be at the initial
+        # state of our car
+        POS[initCarX, initCarY, 0] = 0
+
+        # propagating them forward through the time dimesion of the occupancy
+        # set
+        for time in range(0, maxTime):
+            vbuf = time * minAllowedVelInLane
+            POS[ii, (vbuf + 1):, time + 1] = POS[ii, 1:-vbuf, 1]
+
+    return POS
